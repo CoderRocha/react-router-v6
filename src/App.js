9b682abcac41f1fbc5c1b6303f0b800799bc5f9f
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-20 18:13:49 -0300

    updated company name

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <nav>
-          <h1>The Ninja Clothing Company</h1>
+          <h1>The Quality Clothing Company</h1>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
